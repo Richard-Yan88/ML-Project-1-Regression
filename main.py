9990dba335c51792caaaa11e9db600d727080397
RichardYan88 <pianoguy37@gmail.com>
2022-08-29 07:47:55 -0700

    modifying

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import pandas as pd
 import cv2
 import sklearn
 
-print(pd)
 
 data = pd.read_csv(r'/Users/richardyan/Documents/COMPUTER SCIENCE/ML Project 1 Regression\student-mat.csv')
 data = data [["G1","G2","G3", "studytime","", "failures", "absences"]]
